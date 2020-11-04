a487f3ebdeff78d96ed8fca93db15b2800802646
Natalia Martian <natalia.martian@gmail.com>
2020-11-04 04:34:23 +0000

    Linted function

--- survey.js
+++ survey.js
@@ -7,10 +7,10 @@ const rl = readline.createInterface({
 
 const answers = [];
 
-rl.question("What\'s your name? Nicknames are also acceptable :)  ", (answer0) => {
+rl.question("What's your name? Nicknames are also acceptable :)  ", (answer0) => {
   answers.push(answer0);
 
-  rl.question("What\'s an activity you like doing?  ", (answer1) => {
+  rl.question("What's an activity you like doing?  ", (answer1) => {
     answers.push(answer1);
 
     rl.question("What do you listen to while doing that?  ", (answer2) => {
@@ -19,7 +19,7 @@ rl.question("What\'s your name? Nicknames are also acceptable :)  ", (answer0) =
       rl.question("Which meal is your favourite (eg: dinner, brunch, etc.)  ", (answer3) => {
         answers.push(answer3);
         
-        rl.question("What\'s your favourite thing to eat for that meal?  ", (answer4) => {
+        rl.question("What's your favourite thing to eat for that meal?  ", (answer4) => {
           answers.push(answer4);
           
           rl.question("Which sport is your absolute favourite?  ", (answer5) => {
@@ -28,17 +28,14 @@ rl.question("What\'s your name? Nicknames are also acceptable :)  ", (answer0) =
             rl.question("What is your superpower? In a few words, tell us what you are amazing at!  ", (answer6) => {
               answers.push(answer6);
               
-              // console.log(answers);
               console.log(`Here is your online profile:\n\n${answers[0]} loves listening to ${answers[2]} while ${answers[1]}, devouring ${answers[4]} for ${answers[3]}, prefers ${answers[5]} over any other sport, and is amazing at ${answers[6]}.`);
               rl.close();
         
-            });   
+            });
           });
         });
       });
     });
   });
 });
-
-// console.log(`Here is your online profile:\n\n ${answers[0]} loves listening to ${answers[2]} while ${answers[1]}, devouring ${answers[4]} for ${answers[3]}, prefers ${answers[5]} over any other sport, and is amazing at ${answers[6]}.`);
 
